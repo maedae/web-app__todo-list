9d49e884fd60e734b234b51c5f77b35c208517ba
Meagan Jungman <meagan.jungman@gmail.com>
2016-02-24 12:19:05 -0600

    an add a user. gets an error message with instructions if any combination of the three fields are not filled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,24 @@
-# This controller is for all the CRUD operations related to a User.
+# controller used for the creation of a new user.
+# page contains new user form. 
+MyApp.get "/users/create" do
+  erb :"/users/create_user"
+end
+
+MyApp.post "/users/create/confirmation" do
+  @new_user = User.new
+  @new_user.name = params[:name]
+  @new_user.email = params[:email]
+  @new_user.password = params[:password]
+
+  @error_check = @new_user.create_user_check_valid_action
+
+  if @error_check.empty? == true
+    @new_user.save
+    session[:user_id] = @new_user.id  
+    erb :"/main"
+  else
+    @error = true
+    erb :"/users/create_user"
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,51 @@
 class User < ActiveRecord::Base
+  # RETURNS boolean if 
+  def check_create_user_email_is_valid
+    return self.email != "" ? true : "email"
+  end
+
+  def check_create_user_name_is_valid
+    return self.name != "" ? true : "name"
+  end
+
+  def check_create_user_password_is_valid
+    return self.password != "" ? true : "password"
+  end
+
+
+  def create_user_check_valid_action
+    email = check_create_user_email_is_valid
+    name =  check_create_user_name_is_valid
+    password = check_create_user_password_is_valid
+    message = []
+
+    if name == "name"
+      message << user_name_error
+    end
+
+    if email == "email"
+      message << user_email_error
+    end
+
+    if password == "password"
+      message << user_password_error
+    end
+    
+    return message
+  end
+
+
+  def user_name_error
+    return "Please include your name."
+  end
+
+  def user_email_error
+    return "Please include your email."
+  end
+
+  def user_password_error
+    return "Please include a password."
+  end
+
 end
+
