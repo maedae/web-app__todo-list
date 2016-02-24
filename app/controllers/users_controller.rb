93ea29679f27b8f6d93a162ab2c9237026af01fb
Meagan Jungman <meagan.jungman@gmail.com>
2016-02-24 12:31:21 -0600

    added tomdoc comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ MyApp.post "/users/create/confirmation" do
 
   @error_check = @new_user.create_user_check_valid_action
 
+  # checks to see if error array value is empty. If it is, they are sent to a customized welcome page. If it isn't
+  # user is sent back to the creation page and will see customized error message instructions.
   if @error_check.empty? == true
     @new_user.save
     session[:user_id] = @new_user.id  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,48 +1,63 @@
 class User < ActiveRecord::Base
-  # RETURNS boolean if 
+  # RETURNS boolean if email value String is not empty or not
   def check_create_user_email_is_valid
-    return self.email != "" ? true : "email"
+    return self.email != "" ? true : false
   end
 
+  # RETURNS boolean if name value String is not empty or not
   def check_create_user_name_is_valid
-    return self.name != "" ? true : "name"
+    return self.name != "" ? true : false
   end
 
+  # RETURNS boolean if password value String is not empty or not
   def check_create_user_password_is_valid
-    return self.password != "" ? true : "password"
+    return self.password != "" ? true : false
   end
 
-
+  # Method looks at boolean values from other methods to determine what sign up errors have appeared. 
+  #Calls other methods if values are false
+  #
+  # email  - calls on pre-existing method. stores Boolean return value
+  # name - calls on pre-existing method. stores Boolean return value
+  # password - calls on pre-existing method. stores Boolean return value
+  # message - empty Array. will store Strings via method return values when applicable
+  # user_name_error - Method returns String value
+  # user_email_error - Method returns String value
+  # user_password_error -Method returns String value
+  #
+  # Returns Array containing 3 - 0 String elements, depending on Method algorithm outcome
   def create_user_check_valid_action
     email = check_create_user_email_is_valid
     name =  check_create_user_name_is_valid
     password = check_create_user_password_is_valid
     message = []
 
-    if name == "name"
+    if name == false
       message << user_name_error
     end
 
-    if email == "email"
+    if email == false
       message << user_email_error
     end
 
-    if password == "password"
+    if password == false
       message << user_password_error
     end
     
     return message
   end
 
-
+  # RETURNS String
   def user_name_error
     return "Please include your name."
   end
 
+  # RETURNS String
   def user_email_error
     return "Please include your email."
   end
 
+  # RETURNS String
   def user_password_error
     return "Please include a password."
   end
